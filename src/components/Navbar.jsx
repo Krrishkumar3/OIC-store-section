ca3bb0ab91a4b0e1114de7b1c32ee2d1ef75e4cd
krrish kumar <kumarkrrish724@gmail.com>
2025-11-01 17:22:11 +0530

    fixed mobile ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="logo-section">
@@ -21,12 +23,22 @@ const Navbar = () => {
         </div>
       </div>
 
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
+      {/* Hamburger icon for mobile */}
+      <div
+        className={`hamburger ${menuOpen ? "open" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+        <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
 
         {/* Auction dropdown */}
         <li className="dropdown">
-          <Link to="/auction" className="dropbtn">Auction ▾</Link>
+          <Link to="/auction" className="dropbtn" onClick={() => setMenuOpen(false)}>Auction ▾</Link>
           <div className="dropdown-content">
             <Link to="/auction/notice">Notice</Link>
             <Link to="/auction/guideline">Guideline</Link>
@@ -35,16 +47,16 @@ const Navbar = () => {
 
         {/* Download dropdown */}
         <li className="dropdown">
-          <Link to="/download" className="dropbtn">Download ▾</Link>
+          <Link to="/download" className="dropbtn" onClick={() => setMenuOpen(false)}>Download ▾</Link>
           <div className="dropdown-content">
             <Link to="/download/sop">SOP</Link>
             <Link to="/download/guideline">Guideline</Link>
           </div>
         </li>
 
-        <li><Link to="/staff">Staff</Link></li>
-        <li><Link to="/policies">Policies</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/staff" onClick={() => setMenuOpen(false)}>Staff</Link></li>
+        <li><Link to="/policies" onClick={() => setMenuOpen(false)}>Policies</Link></li>
+        <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
       </ul>
     </nav>
   );
