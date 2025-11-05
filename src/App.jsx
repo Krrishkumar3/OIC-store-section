d8f50937117c483c53c9cf83fa01b171dbf90983
krrish kumar <kumarkrrish724@gmail.com>
2025-11-05 10:19:39 +0530

    fixed the footer to stay permanently at the bottom

--- src/App.jsx
+++ src/App.jsx
@@ -15,37 +15,42 @@ import Staff from "./pages/Staff";
 import Policies from "./pages/Policies";
 import Contact from "./pages/Contact";
 
+import "./App.css"; // 👈 Make sure this import is here
+
 function App() {
   return (
-    <>
+    <div className="page-wrapper">
       <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Hero />
-              <VCMessage imageSrc={"/prateeksharmadtu.png"} />
-            </>
-          }
-        />
-        {/* Auction and its subsections */}
-        <Route path="/auction" element={<Auction />} />
-        <Route path="/auction/notice" element={<AuctionNotice />} />
-        <Route path="/auction/guideline" element={<AuctionGuideline />} />
+      <main className="content">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <VCMessage imageSrc={"/prateeksharmadtu.png"} />
+              </>
+            }
+          />
 
-        {/* Download and its subsections */}
-        <Route path="/download" element={<Download />} />
-        <Route path="/download/sop" element={<SOP />} />
-        <Route path="/download/guideline" element={<DownloadGuideline />} />
+          {/* Auction and its subsections */}
+          <Route path="/auction" element={<Auction />} />
+          <Route path="/auction/notice" element={<AuctionNotice />} />
+          <Route path="/auction/guideline" element={<AuctionGuideline />} />
 
-        {/* Other pages */}
-        <Route path="/staff" element={<Staff />} />
-        <Route path="/policies" element={<Policies />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+          {/* Download and its subsections */}
+          <Route path="/download" element={<Download />} />
+          <Route path="/download/sop" element={<SOP />} />
+          <Route path="/download/guideline" element={<DownloadGuideline />} />
+
+          {/* Other pages */}
+          <Route path="/staff" element={<Staff />} />
+          <Route path="/policies" element={<Policies />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
